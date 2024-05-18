9087c779e0e1740bed936beac7835a26f14399eb
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2024-05-18 22:54:42 +0200

    Revert migracije jer pravi problem

--- Laravel/app/Http/Controllers/UserController.php
+++ Laravel/app/Http/Controllers/UserController.php
@@ -30,13 +30,13 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $validatedData = $request->validate([
-            'username' => 'required|string|max:32',
+            'name' => 'required|string|max:32',
             'email' => 'required|string|email|max:64|unique:users',
             'password' => 'required|string|min:4',
         ]);
 
         $user = User::create([
-            'username' => $validatedData['username'],
+            'name' => $validatedData['name'],
             'email' => $validatedData['email'],
             'password' => bcrypt($validatedData['password']),
         ]);
@@ -76,13 +76,13 @@ class UserController extends Controller
         }
 
         $validatedData = $request->validate([
-            'username' => 'sometimes|required|string|max:32',
+            'name' => 'sometimes|required|string|max:32',
             'email' => 'sometimes|required|string|email|max:64|unique:users,email,' . $user->id,
             'password' => 'sometimes|required|string|min:4',
         ]);
 
-        if (isset($validatedData['username'])) {
-            $user->name = $validatedData['username'];
+        if (isset($validatedData['name'])) {
+            $user->name = $validatedData['name'];
         }
         if (isset($validatedData['email'])) {
             $user->email = $validatedData['email'];
